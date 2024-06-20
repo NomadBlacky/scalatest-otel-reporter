89f906e0723f692598860e6f15642031540c4eda
NomadBlacky <nomadblacky@gmail.com>
2024-06-21 00:20:59 +0900

    scalafmt

--- scalatest-otel-reporter/src/main/scala/dev/nomadblacky/scalatest_otel_reporter/OpenTelemetrySdkTestReporter.scala
+++ scalatest-otel-reporter/src/main/scala/dev/nomadblacky/scalatest_otel_reporter/OpenTelemetrySdkTestReporter.scala
@@ -5,7 +5,7 @@ import java.util.logging.Logger
 
 trait OpenTelemetrySdkTestReporter extends OpenTelemetryTestReporter[OpenTelemetrySdk] {
   private lazy val logger = Logger.getLogger(classOf[OpenTelemetrySdkTestReporter].getName)
-  
+
   override protected def shutdownOtel(): Unit = {
     logger.info("Shutting down OpenTelemetry")
     otel.close()
